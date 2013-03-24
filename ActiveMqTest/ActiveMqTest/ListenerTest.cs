e3b8590f3c32b70392aa79fbbdf4cde33949c2dc
pitsanu <p.tongprasin@gmail.com>
2013-03-24 16:52:39 +0700

    Try to create MQ, not success!
    Signed-off-by: pitsanu <p.tongprasin@gmail.com>

---
+++ ActiveMqTest/ActiveMqTest/ListenerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Spring.Messaging.Nms.Core;
+using Apache.NMS;
+
+namespace ActiveMqTest
+{
+    class ListenerTest : IMessageListener
+    {
+        public ListenerTest()
+        {
+            Console.WriteLine("Listener Ver 0.1.0 Activated");            
+        }
+
+        public void OnMessage(Apache.NMS.IMessage message)
+        {
+            Console.WriteLine("OnMessage");
+            //ITextMessage textMessage = message as ITextMessage;
+
+            //if (textMessage == null) Console.WriteLine("Message is NULL or EMPTY");
+            //else
+            //Console.WriteLine("You have got the Message : " + textMessage.Text);
+        }
+    }
+}
---
+++ ActiveMqTest/ActiveMqTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Apache.NMS.ActiveMQ;
+using Spring.Messaging.Nms.Listener;
+
+namespace ActiveMqTest
+{
+    class Program
+    {
+        private const string URI = "tcp://localhost:61616";
+        private const string queueName = "activemqtest.queue";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                ConnectionFactory connectionFactoryTest = new ConnectionFactory(URI);
+               
+                using (SimpleMessageListenerContainer listenerTest = new SimpleMessageListenerContainer())
+                {
+                    listenerTest.ConnectionFactory = connectionFactoryTest;
+                    listenerTest.DestinationName = queueName;
+                    listenerTest.MessageListener = new ListenerTest();
+                    listenerTest.AfterPropertiesSet();
+                    listenerTest.Start();
+
+                    Console.WriteLine("Listening to Queue.");
+                    Console.WriteLine("[Main]: Press <ENTER> to exit.");
+                    Console.Read();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);                
+                Console.WriteLine("[Exception]: Press <ENTER> to exit.");
+                Console.Read();
+            }
+        }
+    }
+}
